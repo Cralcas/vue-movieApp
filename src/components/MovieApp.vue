8cda20947abc78bb4260bbb736026321ae2b64d4
Cralcas <carlosbergq@gmail.com>
2024-05-13 19:23:23 +0200

    loop through movies

--- src/components/MovieApp.vue
+++ src/components/MovieApp.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import { IMovie } from "@/models/IMovie";
 import { getMovies } from "@/services/movieService";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+
+const movies = ref<IMovie[]>([]);
 
 onMounted(async () => {
-  await getMovies("bad");
+  movies.value = await getMovies("bad");
 });
 </script>
 
 <template>
-  <h2>Hello!</h2>
+  <div>
+    <h2>Movies:</h2>
+    <ul>
+      <li v-for="movie in movies" :key="movie.imdbID">{{ movie.Title }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
