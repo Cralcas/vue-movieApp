4ea43fd8e0c67ecec197e174c84b20b1edcee090
Cralcas <carlosbergq@gmail.com>
2024-05-14 10:40:49 +0200

    handleSearch

--- src/components/MovieApp.vue
+++ src/components/MovieApp.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import { IMovie } from "@/models/IMovie";
 import { getMovies } from "@/services/movieService";
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
+
+import MovieSearch from "./MovieSearch.vue";
 
 const movies = ref<IMovie[]>([]);
 
-onMounted(async () => {
-  movies.value = await getMovies("bad");
-});
+const handleSearch = async (searchText: string) => {
+  movies.value = await getMovies(searchText);
+};
 </script>
 
 <template>
   <div>
+    <MovieSearch @movie-search="handleSearch" />
     <h2>Movies:</h2>
     <ul>
       <li v-for="movie in movies" :key="movie.imdbID">{{ movie.Title }}</li>
---
+++ src/components/MovieSearch.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const searchText = ref("");
+
+const emit = defineEmits<{ (e: "movieSearch", text: string): void }>();
+
+const handleSubmit = () => {
+  if (searchText.value !== "") {
+    emit("movieSearch", searchText.value);
+  } else {
+    console.log("Input får inte vara tom");
+  }
+};
+</script>
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" v-model="searchText" />
+    </form>
+  </div>
+</template>
