301255e4d9a5e892569ed6ddcdba693c35baf0db
Cralcas <carlosbergq@gmail.com>
2024-05-14 14:57:46 +0200

    modal component

--- src/components/MovieApp.vue
+++ src/components/MovieApp.vue
@@ -2,13 +2,19 @@
 import { IMovie } from "@/models/IMovie";
 import { getMovies } from "@/services/movieService";
 import { ref } from "vue";
-
 import MovieSearch from "./MovieSearch.vue";
+import MovieModal from "./MovieModal.vue";
 
 const movies = ref<IMovie[]>([]);
 
 const handleSearch = async (searchText: string) => {
   movies.value = await getMovies(searchText);
+};
+
+const isModalOpened = ref(false);
+
+const closeModal = () => {
+  isModalOpened.value = false;
 };
 </script>
 
@@ -19,6 +25,9 @@ const handleSearch = async (searchText: string) => {
     <ul>
       <li v-for="movie in movies" :key="movie.imdbID">{{ movie.Title }}</li>
     </ul>
+  </div>
+  <div>
+    <MovieModal :isOpen="isModalOpened" @modal-close="closeModal"></MovieModal>
   </div>
 </template>
 
---
+++ src/components/MovieModal.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+
+const open = ref(false);
+</script>
+
+<template>
+  <button @click="open = true">Open Modal</button>
+
+  <Teleport to="body">
+    <div v-if="open" class="modal">
+      <div>
+        <h3>Movie Title</h3>
+        <button @click="open = false">Close</button>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style scoped>
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.1);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal > div {
+  background-color: #fff;
+  padding: 50px;
+  border-radius: 10px;
+}
+</style>
